b51041e7ecfbc8aee70f962e3122820e19219eff
Shubham Gupta <shubham.gupta@scripbox.com>
2019-03-25 11:42:38 +0530

    Load file using skimage before example creation
    Use skimage.io to load jpg files before creating an example as some
    files in the Jaderberg dataset are invalid jpg images. These images
    break the training process with the following error:
    
    Premature end of JPEG data. Stopped at line 0/31

--- data/create_tfrecord.py
+++ data/create_tfrecord.py
@@ -1,57 +1,25 @@
-import os
-import io
 import tensorflow as tf
 import standard_fields as fields
 import cv2
-import scipy
 import dataset_util
+import skimage
 
 
 flags = tf.app.flags
 flags.DEFINE_string(
-    'output_path', '/Users/shubham/shubham/amrita_research/AON/data/train.tfrecord', 'tfrecord filename')
+    'output_path',
+    '/Users/shubham/shubham/amrita_research/AON/data/train.tfrecord',
+    'tfrecord filename'
+)
 flags.DEFINE_string(
-    'tags_file_path', '/Users/shubham/shubham/amrita_research/AON/data/train.txt', 'tags file file')
+    'tags_file_path',
+    '/Users/shubham/shubham/amrita_research/AON/data/train.txt',
+    'tags file file'
+)
 FLAGS = flags.FLAGS
 
 
 def main(unused_argv):
-    writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
-    count = 0
-
-    with open(FLAGS.tags_file_path) as fo:
-        for line in fo:
-            image_path = line.split(' ')[0].strip()
-            filename = '/'.join(line.strip().split('/')[-2:])
-            groundtruth_text = line.split('_')[1]
-            try:
-                height, width, channel = cv2.imread(image_path).shape
-                image_bin = open(image_path, 'rb').read()
-                example = tf.train.Example(features=tf.train.Features(
-                    feature={
-                        fields.TfExampleFields.image_encoded: dataset_util.bytes_feature(image_bin),
-                        fields.TfExampleFields.height: dataset_util.int64_feature(height),
-                        fields.TfExampleFields.width: dataset_util.int64_feature(width),
-                        fields.TfExampleFields.filename: dataset_util.bytes_feature(filename.encode()),
-                        fields.TfExampleFields.transcript: dataset_util.bytes_feature(groundtruth_text.encode())
-                    }
-                ))
-
-                writer.write(example.SerializeToString())
-                count += 1
-                if count % 1000 == 0:
-                    print(count)
-
-            except Exception as e:
-                print(e)
-                print("Failed for filename: ", filename)
-                continue
-
-    writer.close()
-    print('{} example finished!'.format(count))
-
-
-def make_tfrecord_from_tags(unused_argv):
     writer = tf.python_io.TFRecordWriter(FLAGS.output_path)
     count = 0
 
@@ -63,6 +31,7 @@ def make_tfrecord_from_tags(unused_argv):
             groundtruth_text = ts[1].strip()
 
             try:
+                skimage.io.imread(image_path)
                 height, width, channel = cv2.imread(image_path).shape
                 image_bin = open(image_path, 'rb').read()
             except Exception as e:
@@ -72,17 +41,22 @@ def make_tfrecord_from_tags(unused_argv):
 
             example = tf.train.Example(features=tf.train.Features(
                 feature={
-                    fields.TfExampleFields.image_encoded: dataset_util.bytes_feature(image_bin),
-                    fields.TfExampleFields.height: dataset_util.int64_feature(height),
-                    fields.TfExampleFields.width: dataset_util.int64_feature(width),
-                    fields.TfExampleFields.filename: dataset_util.bytes_feature(filename.encode()),
-                    fields.TfExampleFields.transcript: dataset_util.bytes_feature(groundtruth_text.encode())
+                    fields.TfExampleFields.image_encoded:
+                        dataset_util.bytes_feature(image_bin),
+                    fields.TfExampleFields.height:
+                        dataset_util.int64_feature(height),
+                    fields.TfExampleFields.width:
+                        dataset_util.int64_feature(width),
+                    fields.TfExampleFields.filename:
+                        dataset_util.bytes_feature(filename.encode()),
+                    fields.TfExampleFields.transcript:
+                        dataset_util.bytes_feature(groundtruth_text.encode())
                 }
             ))
             writer.write(example.SerializeToString())
             count += 1
 
-            if count % 1000 == 0:
+            if count % 100000 == 0:
                 print(count)
 
     writer.close()
@@ -90,4 +64,4 @@ def make_tfrecord_from_tags(unused_argv):
 
 
 if __name__ == '__main__':
-    tf.app.run(main=make_tfrecord_from_tags)
+    tf.app.run()
